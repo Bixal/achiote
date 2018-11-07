7265280b90502e341d6d5415a5f1fbef3bbe5f4c
Abby Milberg <abby.milberg@bixal.com>
2018-11-07 08:57:57 -0500

    Updates to the gulp setup

--- README.md
+++ README.md
@@ -3,4 +3,38 @@ Bixal's Drupal 8 starter theme.
 
 Trivia: "Achiote" is the name of the plant whose scientific name (_Bixa orellana_) provided inspiration for Bixal's name.
 
+## To Get Started:
+
+1. If they aren't already, make sure that node and npm (Node Package Manager) are [installed](http://blog.nodeknockout.com/post/65463770933/how-to-install-node-js-and-npm). If you are using a Mac, Homebrew is recommended for this.
+2. If this is your first time ever using gulp, you will need to install it globally by running `npm install -g gulp` so that the terminal recognizes gulp commands.
+3. The first time running this particular project, `cd` into this directory and type `npm install`
+4. To continually watch .scss and .js files for changes, type `gulp` and use `ctrl + C` to stop.
+5. To run a one-time compilation of .scss files, type `gulp scss`
+
+*Note:* You'll need the [livereload browser extension](http://livereload.com/extensions/) to automatically refresh on changes.
+
+## Guidelines:
+
+* In general, all units should use the `px-to-rem()` described in scss/base/_functions.scss
+* With rare case-by-case exceptions, bitmap sprites should never be used.
+* For multi-color illustrations for which a bitmap sprite might often be used, use an SVG sprite instead if at all possible.
+* Do not nest your CSS selectors, except for states (:hover, etc.), pseudoelements(:before, :after), or pseudo-selectors (:nth-child, etc.). While SCSS provides this functionality, it can easily get out of control creating hard-to-read code and unwieldy selectors.
+* All styling should be done mobile-first. This repo contains a mixin for the `min-width` media query only, which in the vast majority of cases should be all you need.
+* When in doubt, break your code out into its own partial. You can add partials by first creating the partial and then importing it from _components.scss, _base.scss, _layout.scss, or _global.scss. In the majority of cases, it will belong in components. Do not put partials outside of these folders.
+
+
+## Generating Icons
+* Add your icons to `images/icons/` and run the `gulp icons` task.
+  This will optimize your SVGs and create an icon font with scss partial.
+* Tip: If your icons seem tiny or invisible despite normal font sizes, make sure that none of the svg files have `width="100%"` and/or `height="100%"` on the `<svg>` tag. In a pinch, if you're not sure what part of your svg's code is messing up your font, try uploading the svgs to icomoon.io and re-downloading them.
+
+## Sass-lint
+Sass-lint accepts `YML` or `JSON` files. Config needs to be in project root unless you specify in `package.json`.
+
+If you need to ignore a rule use: `// sass-lint:disable <rule>, <rule>` and include a reason.
+
+[Docs here](https://github.com/sasstools/sass-lint/tree/develop/docs)
+
+---
+
 Screenshot image: ["Achiote (bixa orellana)"](https://www.flickr.com/photos/missy-and-the-universe/3493687779/in/photolist-6jJ4Ec-21RRh17-dJARfG-sstPg-6a9Se6-rtovr-b8v5pz-t8cDMZ-eLqB2e-2a3kLPK-bn5e8Z-6BZfPz-sstHG-gFXVs3-aTveG-6jJ3zT-pmnJK8-NR7uFm-6rdZ4T-dMwxYo-p4SvNK-bAAJGg-7S5VNT-e7qnzh-6a9SdZ-DQ23q4-dRYMoZ-DTLehr-DrbesP-6Y4G8h-EfkkeX-85jZ6a-8iviH5-rd6Ph-c1kn3m-DrDW9G-DrbduB-DqMeMy-7PyqfG-DTLd4V-5VNNyc-b8VEqg-DKwc2s-7S5Vrn-Dr7VYr-MdLLiX-JV9Mnn-23bi23N-MdLJJ4-yKCYp8 ) by [missy](https://www.flickr.com/people/missy-and-the-universe/) is licensed under [CC BY 2.0](https://creativecommons.org/licenses/by/2.0/legalcode) / Cropped from original
--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,157 @@
-var gulp        = require('gulp'),
-    sass        = require('gulp-sass'),
-    prefix      = require('autoprefixer'),
-    notify      = require('gulp-notify'),
-    postcss     = require('gulp-postcss'),
-    imagemin    = require('gulp-imagemin'),
-    iconfont    = require('gulp-iconfont'),
-    iconfontCSS = require('gulp-iconfont-css'),
-    sourcemaps  = require('gulp-sourcemaps'),
-    cssnano     = require('gulp-cssnano');
-    // babel
-    // sass linting
-    // js linting
+var gulp         = require('gulp'),
+    sass         = require('gulp-sass'),
+    eslint       = require('gulp-eslint'),
+    scsslint     = require('gulp-sass-lint'),
+    cache        = require('gulp-cached'),
+    prefix       = require('autoprefixer'),
+    notify       = require('gulp-notify'),
+    postcss      = require('gulp-postcss'),
+    imagemin     = require('gulp-imagemin'),
+    iconfont     = require('gulp-iconfont'),
+    iconfontCSS  = require('gulp-iconfont-css'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    cssnano      = require('gulp-cssnano'),
+    plumber      = require('gulp-plumber'),
+    sassGlob     = require('gulp-sass-glob'),
+    babel        = require('gulp-babel'),
+    browserSync  = require('browser-sync'),
+    reload       = browserSync.reload,
+    beeper       = require('beeper'),
+    runTimestamp = Math.round(Date.now() / 1000);
 
 // Prefix with project code
 var fontName = 'icons';
 
-gulp.task('scss', function() {
+// Paths
+var paths = {
+  styles: [
+    'scss/**/*.scss'
+  ],
+  scripts: [
+    'js/*.js'
+  ],
+  images: {
+    src: './images/**/*',
+    svg: './images/icons/*.svg'
+  }
+}
+
+gulp.task('scss', () => {
   return gulp.src('scss/styles.scss')
+    .pipe(plumber({ errorHandler: function(err) {
+      notify.onError({
+        title: "Gulp error in " + err.plugin,
+        message:  err.toString()
+      })(err);
+      beeper();
+    }}))
     .pipe(sourcemaps.init())
-    .pipe(sass())
-    .on('error', notify.onError({
-      title:    "Gulp",
-      subtitle: "Failure!",
-      message:  "Error: <%= error.message %>"
-    }))
+    .pipe(sassGlob())
+    .pipe(sass({outputStyle: 'compressed'}))
     .pipe(cssnano({zindex: false}))
-    .pipe(sourcemaps.write())
-    .pipe(postcss([ prefix({ browsers: ['last 2 versions'], cascade: false }) ]))
-    .pipe(gulp.dest('css'));
+    .pipe(postcss([
+      prefix({
+        browsers: ['last 3 versions'],
+        cascade: false })
+      ]))
+    .pipe(sourcemaps.write('../css/maps'))
+    .pipe(gulp.dest('css'))
+    .pipe(reload({stream:true}));
 });
 
-gulp.task('optimize-images', function() {
-  gulp.src('./images/**/*', {base: '.'})
+gulp.task("scsslint", () => {
+  return gulp.src(paths.styles)
+    .pipe(
+      scsslint({
+        options: {
+          configFile: "sass-lint.yml"
+        }
+      })
+    )
+    .pipe(scsslint.format())
+    .pipe(scsslint.failOnError());
+});
+
+gulp.task('optimize-images', () => {
+  return gulp.src(paths.images.src, {base: '.'})
     .pipe(imagemin());
 });
 
-gulp.task('iconfont', function() {
-  gulp.src(['./images/icons/*.svg'])
+gulp.task('iconfont', () => {
+  return gulp.src(paths.images.svg)
     .pipe(iconfontCSS({
       fontName: fontName,
       path: './scss/templates/icons.scss',
       targetPath: '../scss/global/_icons.scss',
       fontPath: '../fonts/',
+      cacheBuster: runTimestamp
     }))
     .pipe(iconfont({
       fontName: fontName,
       // Remove woff2 if you get an ext error on compile
       formats: ['svg', 'ttf', 'eot', 'woff', 'woff2'],
       normalize: true,
-      fontHeight: 1001
+      fontHeight: 1001,
+      prependUnicode: true,
+      timestamp: runTimestamp,
     }))
     .pipe(gulp.dest('./fonts/'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('scss/**/*.scss', ['scss']);
+gulp.task('eslint', () => {
+  gulp.src(paths.scripts)
+    .pipe(eslint({
+      parser: 'babel-eslint',
+      rules: {
+        'no-mutable-exports': 0,
+      },
+      globals: [
+        'jQuery',
+        '$',
+      ],
+      envs: [
+        'browser',
+      ]
+    }))
+    .pipe(eslint.format());
 });
 
-gulp.task('icons', ['optimize-images', 'iconfont', 'scss']);
-gulp.task('build', ['icons']);
-gulp.task('default', ['scss', 'watch']);
+gulp.task('scripts', () => {
+  return gulp.src(paths.scripts)
+      .pipe(plumber({ errorHandler: function(err) {
+        notify.onError({
+          title: "Gulp error in " + err.plugin,
+          message: err.toString()
+        })(err);
+        beeper();
+      }}))
+      .pipe(babel({
+        presets: ['env'],
+      }))
+      .pipe(plumber())
+      .pipe(sourcemaps.init())
+      .pipe(sourcemaps.write('./maps'))
+      .pipe(gulp.dest('js/dist'))
+      .pipe(reload({stream:true}));
+});
+
+// Browser Sync
+gulp.task('browser-sync', () => {
+  browserSync({
+    proxy: {
+      // Update this with your project's local url
+      target: "http://local.yourlocal.com"
+    }
+  });
+});
+
+gulp.task('styles', gulp.series('scss', 'scsslint'));
+
+gulp.task('watch', () => {
+  gulp.watch(paths.styles, gulp.series('styles')).on('change', reload);
+  gulp.watch(paths.scripts, gulp.series('scripts', 'eslint')).on('change', reload);
+});
+
+gulp.task('icons', gulp.series('optimize-images', 'iconfont', 'styles'));
+gulp.task('default', gulp.parallel('styles', 'browser-sync', 'watch'));
+// @todo add a build task - will need to rethink where reload is being called
